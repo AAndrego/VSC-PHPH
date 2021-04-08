704d371b9a339f7bd132083098f451cd477f166c
Xiomara150818 <xjimenez@ucundinamarca.edu.co>
2021-04-07 22:16:44 -0500

    diseño

--- rangos.php
+++ rangos.php
@@ -11,26 +11,21 @@
 </head>
 
 <body>
-
     <section>
-        <h3>Rangos:</h3>
+        <center><font face= "Brush Script MT"><FONT SIZE= "6" ><H1 style="color: #40604D;"> Rangos</H1></FONT></font></center>
 	    <form name="modificacion"  method="get" action="modimpu.php">
 		
-        <p><label>Limite rango 1:</label>
-		
-        <input type="text" name="Rang1"><label>   Porcentaje 1: </label><input type="text" name="Por1"></p>
+        <center><font face= "Century Gothic"> <font size="4"><p><label>Limite rango 1:</label>
+        <input type="text" name="Rang1"><label>   Porcentaje 1: </label><input type="text" name="Por1"></p></font></font></center>
         
-        <p><label>Limite rango 2:</label>
+        <center><font face= "Century Gothic"> <font size="4"><p><label>Limite rango 2:</label>
+        <input type="text" name="Rang2"><label>   Porcentaje 2: </label><input type="text" name="Por2"></p></font></font></center>
 		
-        <input type="text" name="Rang2"><label>   Porcentaje 2: </label><input type="text" name="Por2"></p>
-		
-        <p><label>Limite rango 3:</label>
-		
-        <input type="text" name="Rang3"><label>   Porcentaje 3: </label><input type="text" name="Por3"></p>
+        <center><font face= "Century Gothic"> <font size="4"><p><label>Limite rango 3:</label>
+        <input type="text" name="Rang3"><label>   Porcentaje 3: </label><input type="text" name="Por3"></p></font></font></center>
 
-        <p> Porcentaje 4: </label><input type="text" name="Por4"></p>
-
-        <input type = "submit" value="Guardar">  
+        <center><font face= "Century Gothic"> <font size="4"><p> Porcentaje 4: </label><input type="text" name="Por4"></p></font></font></center>
+        <center><font face= "Century Gothic"> <font size="4"><input type = "submit" value="Guardar"></font></font></center>
         
 
         </form>
@@ -38,7 +33,9 @@
     </section>
 
 	<footer>
-	
+    <CENTER><H5>GUIA 3 Ingenieria de software II</H5></CENTER>
+    <CENTER><H5>ING. Fernando Sotelo</H5></CENTER>
+    <CENTER><h6>Xiomara Fernanda Jimenez Aya - Anderson Andres Gonzalez Hernandez - Jeisson Felipe Torres Huerfano</h6></CENTER>
 	</footer>
 	
 	</body>
