daf1be80636ed179e303566dd64679d947d8bdba
Xiomara150818 <xjimenez@ucundinamarca.edu.co>
2021-04-07 21:30:50 -0500

    diseño

--- index.php
+++ index.php
@@ -11,42 +11,43 @@
 </head>
 
 <body>
-
-    <section>
-	    <h1>Calculo de impuestos</h1>
-        <h3>Datos del vehiculo:</h3>
-	    <form name="cuestionario"  method="get" action="guardarimpu.php">
+    <body bgcolor="#87C9A0">
+        <div id= "Contenidos">
+        <header>
+         <center><font face= "Brush Script MT"><FONT SIZE= "8" ><H1 style="color: #40604D;"> Calculo de impuestos</H1></FONT></font></center>
+        </header>
+    
+        <section id= "content">
+	
+        <center><font face= "Brush Script MT"> <font size="6"><h3>Datos del vehiculo:</h3></font></font></center> 
+	   <form name="cuestionario"  method="get" action="guardarimpu.php">
 		
-        <p><label>Marca:</label>
-		
-        <input type="text" name="Marca"></p>
+       <p><label><center><font face= "Century Gothic"><FONT SIZE= "4">Marca:</FONT></FONT></center></label>
+		<center><input type="text" name="Marca"></p></center>
         
-        <p><label>Linea:</label>
+        <p><label><center><font face= "Century Gothic"><FONT SIZE= "4">Linea:</FONT></FONT></center></label>
+        <center><input type="text" name="Linea"></p></center>
 		
-        <input type="text" name="Linea"></p>
-		
-        <p><label>Modelo:</label>
-		
-        <input type="text" name="Modelo"></p>
+        <p><label><center><font face= "Century Gothic"><FONT SIZE= "4">Modelo:</FONT></FONT></center></label>
+        <center><input type="text" name="Modelo"></p></center>
 
-        <p><label>Valor:</label>
-		
-         <input type="text" name="Valor"></p>
+        <p><label><center><font face= "Century Gothic"><FONT SIZE= "4">Valor:</FONT></FONT></center></label>
+        <center><input type="text" name="Valor"></p></center>
 
 		
-		<h3>Descuentos:</h3>
+		<center><font face= "Brush Script MT"> <font size="6"><h3>Descuentos:</h3></font></font></center> 
 		
         <p>
         <label for="op1">
-		<input type="checkbox" id="op1" name="descuento[]" value="a"> Pronto pago
+		<center><font face= "Century Gothic"><input type="checkbox" id="op1" name="descuento[]" value="a"> Pronto pago</FONT></center>
         </label>
 
         <label for="op2">
-		<input type="checkbox" id="op2" name="descuento[]" value="b"> Translado de cuenta
+		<center><font face= "Century Gothic"><input type="checkbox" id="op2" name="descuento[]" value="b"> Translado de cuenta</FONT></center>
 		</label>
         
         <label for="op3">
-		<input type="checkbox" id="op3" name="descuento[]" value="c"> Servicio publico
+		<center><font face= "Century Gothic"><input type="checkbox" id="op3" name="descuento[]" value="c"> Servicio publico</FONT></center>
         </label>
         </p>
         
@@ -57,19 +58,20 @@
     <input type="hidden" name="po2" value="<?php echo "$p2" ?>"/>
     <input type="hidden" name="po3" value="<?php echo "$p3" ?>"/>
     <input type="hidden" name="po4" value="<?php echo "$p4" ?>"/>
-        <input type = "submit" name="enviar" value="enviar">
+    <center><font face= "Century Gothic"><input type = "submit" value="Enviar"></FONT></center>
       
 
         </form>
         <form name="rangos"  method="get" action="rangos.php">
-        <input type = "submit" name="editar" value="editar">
+        <center><font face= "Century Gothic"><input type = "submit" value="Editar"></FONT></center>
         </form>
     </section>
-
 	<footer>
-	
+    <CENTER><H5>GUIA 3 Ingenieria de software II</H5></CENTER>
+    <CENTER><H5>ING. Fernando Sotelo</H5></CENTER>
+    <CENTER><h6>Xiomara Fernanda Jimenez Aya - Anderson Andres Gonzalez Hernandez - Jeisson Felipe Torres Huerfano</h6></CENTER>
 	</footer>
-	
+	</div>
     
 	</body>
 	
